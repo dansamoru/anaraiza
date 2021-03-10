a2d58768fd664a5cdfd5a6db3d210a87382a434f
Samoilov Daniil <dansamoru@ya.ru>
2021-03-10 20:03:06 +0300

    Добавление .gitignore для базы данных

--- parser/settings.py
+++ parser/settings.py
@@ -1,3 +1,10 @@
+import os
+
+
+#  ---  Global  ---
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
 #  ---  Website  ---
 URL_ADDRESS = 'http://seoji.nl.go.kr/landingPage/SearchAjax.do'
 SEARCH_REQUEST = '(연재) 원작자 2021'
@@ -8,4 +15,4 @@ DATABASE_FILE_PATH = '../database.sqlite'
 
 
 #  ---  Output  ---
-OUTPUT_FILE_PATH = 'output.txt'
+OUTPUT_FILE_PATH = '../output.txt'
