ca8a295bb478c64c103029be814f56b04d0b8e09
Samoilov Daniil <dansamoru@ya.ru>
2021-03-10 18:20:36 +0300

    Add database model

--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
 #  ---  Website  ---
 URL_ADDRESS = 'http://seoji.nl.go.kr/landingPage/SearchAjax.do'
 SEARCH_REQUEST = '(연재) 원작자 2021'
+
+
+#  ---  Database  ---
+DATABASE_FILE_PATH = '../database.sqlite'
