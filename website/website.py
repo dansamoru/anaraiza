b7975de4a71b3afd411b236c73f00bb8b75f80ae
Samoilov Daniil <dansamoru@ya.ru>
2021-03-15 08:51:38 +0300

    Исправление ошибок

--- website/website.py
+++ website/website.py
@@ -1,11 +1,39 @@
+import csv
+import time
+
 import requests
 
 
+class Proxy:
+    def __init__(self, proxy_file_path):
+        self.current_proxy = 1
+
+        with open(proxy_file_path, 'r') as proxy_file:
+            self.proxies = []
+            proxy_reader = csv.reader(proxy_file, delimiter=';')
+            for row in proxy_reader:
+                self.proxies.append(['http://' + row[0] + ':' + row[1] + '@' + row[2] + ':' + row[3], 0.0])
+
+    def __str__(self):
+        return self.proxies[self.current_proxy][0]
+
+    def next(self):
+        print(time.strftime('[%X] ', time.localtime()) + '!   Запрос на изменение прокси   !')
+        self.proxies[self.current_proxy][1] = time.time()
+        enter_proxy = self.current_proxy
+        self.current_proxy = (self.current_proxy + 1) % len(self.proxies)
+        while time.time() - self.proxies[self.current_proxy][1] < 60:
+            if self.current_proxy == enter_proxy:
+                raise ConnectionError('Все прокси израсходованы. Добавьте новых или подождите')
+            else:
+                self.current_proxy = (self.current_proxy + 1) % len(self.proxies)
+
+
 class Website:
-    def __init__(self, url_address, search_request, proxy):
+    def __init__(self, url_address, search_request, proxy_file_path):
         self.url_address = url_address
         self.search_request = search_request
-        self.proxy = proxy
+        self.proxy = Proxy(proxy_file_path=proxy_file_path)
 
     def __request__(self, rows: int, start: int = 0, page: int = 1) -> requests.Response:
         data = {
@@ -17,12 +45,20 @@ class Website:
             'fq_select': 'tSrc_total',
             'sort': 'PUBLISH_PREDATE ASC',
         }
-        return requests.post(self.url_address, data=data, proxies=self.proxy)
+        while True:
+            try:
+                response = requests.post(self.url_address, data=data, proxies={'http:': str(self.proxy)})
+                # if not response.ok:
+                #     raise ConnectionResetError('Ошибка подключения к сайту')
+                return response
+            except requests.exceptions.ProxyError:
+                self.proxy.next()
 
     def get_positions(self, rows: int = None, start: int = 0, page: int = 1):
         if rows is None:
             rows = self.get_count()
-        return self.__request__(rows=rows, start=start, page=page).json()
+        pos = self.__request__(rows=rows, start=start, page=page).json()
+        return pos
 
     def get_count(self) -> int:
         return int(self.__request__(rows=0).json()['response']['numFound'])
