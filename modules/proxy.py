1e8fda62b8507eda270d476755ca9fea56ac4ce2
Samoilov Daniil <dansamoru@ya.ru>
2021-04-08 15:46:29 +0300

    Поддержка прокси без авторизации

--- modules/proxy.py
+++ modules/proxy.py
@@ -3,24 +3,31 @@ import time
 
 
 class Proxy:
-    def __init__(self, proxy_file_path):
-        self.current_proxy = 1
-
+    def __init__(self, proxy_file_path, delimiter=':', proxy_start: int = 0):
         with open(proxy_file_path, 'r', encoding='utf-8') as proxy_file:
-            self.proxies = []
-            proxy_reader = csv.reader(proxy_file, delimiter=';')
+            self.__proxies__ = []
+            proxy_reader = csv.reader(proxy_file, delimiter=delimiter)
             for row in proxy_reader:
-                self.proxies.append(['http://' + row[0] + ':' + row[1] + '@' + row[2] + ':' + row[3], 0.0])
+                if len(row) == 4:
+                    self.__proxies__.append(['http://' + row[2] + ':' + row[3] + '@' + row[0] + ':' + row[1], 0.0])
+                elif len(row) == 2:
+                    self.__proxies__.append(['http://' + row[0] + ':' + row[1], 0.0])
+                else:
+                    raise ValueError('Некорректная запись адреса прокси')
+            if proxy_start < len(self.__proxies__):
+                self.__current_proxy__ = proxy_start
+            else:
+                raise ValueError('Номер первого прокси больше количества прокси')
 
     def __str__(self):
-        return self.proxies[self.current_proxy][0]
+        return self.__proxies__[self.__current_proxy__][0]
 
     def next(self):
-        self.proxies[self.current_proxy][1] = time.time()
-        enter_proxy = self.current_proxy
-        self.current_proxy = (self.current_proxy + 1) % len(self.proxies)
-        while time.time() - self.proxies[self.current_proxy][1] < 60:
-            if self.current_proxy == enter_proxy:
+        self.__proxies__[self.__current_proxy__][1] = time.time()
+        enter_proxy = self.__current_proxy__
+        self.__current_proxy__ = (self.__current_proxy__ + 1) % len(self.__proxies__)
+        while time.time() - self.__proxies__[self.__current_proxy__][1] < 60:
+            if self.__current_proxy__ == enter_proxy:
                 raise RuntimeError('Все прокси израсходованы. Добавьте новых или подождите')
             else:
-                self.current_proxy = (self.current_proxy + 1) % len(self.proxies)
+                self.__current_proxy__ = (self.__current_proxy__ + 1) % len(self.__proxies__)
