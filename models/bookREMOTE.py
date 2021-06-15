086f53e9004a189e6602f97480f687ef6f8318fc
Daniil Samoilov <dansamoru@ya.ru>
2021-06-15 14:20:47 +0300

    Remove useless files

--- models/bookREMOTE.py
+++
@@ -1,7 +0,0 @@
-class Book:
-    def __init__(self, korean_name: str, english_name: str, russian_name: str, key: str, isbn: str):
-        self.korean_name = korean_name
-        self.english_name = english_name
-        self.russian_name = russian_name
-        self.key = key
-        self.isbn = isbn
