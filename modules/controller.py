5b5a99230786b53507b41710a1eb7dc88826fa13
Daniil Samoilov <dansamoru@ya.ru>
2021-09-22 08:13:37 +0300

    Add new site support

--- modules/controller.py
+++ modules/controller.py
@@ -1,8 +1,6 @@
 import os
 import traceback
 
-from modules.database import Database
-from modules.registrar import Registrar
 from modules.telegram import Telegram
 from modules.website import Website
 
@@ -10,13 +8,12 @@ BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class Controller:
-    STEP = 100
+    # STEP = 100
 
     def __init__(self, config):
-        self.website = Website(config['Website'],
-                               proxy_file_path=config['Proxy']['PROXY_FILE_PATH'])
+        self.website = Website(config['Website'])
 
-        self.database = Database(config['Database'])
+        self.database = []
 
         self.telegram = Telegram(bot_token=os.environ.get('TELEGRAM_BOT_TOKEN'),
                                  chat_id={'prod-main': os.environ.get('TELEGRAM_CHAT_ID_PROD_MAIN'),
@@ -24,76 +21,67 @@ class Controller:
                                           'debug-all': os.environ.get('TELEGRAM_CHAT_ID_DEBUG_ALL'), }
                                  )
 
-        self.registrar = Registrar(config['Remanga'])
+        # self.registrar = Registrar(config['noveupdates'])
 
         self.view_url = config['Website']['base_url'] + config['Website']['view_url']
 
-        self.is_database_filled = False
+        # self.is_database_filled = False
 
-    def __del__(self):
-        if not self.is_database_filled:
-            self.database.drop()
+    # def __del__(self):
+        # if not self.is_database_filled:
+        #     self.database.drop()
 
-    def __database_filling__(self, website_count: int):
-        start_count = 0
-        len_count = website_count - start_count
-        if len_count > self.STEP:
-            len_count = self.STEP
-        data = []
-        while start_count + len_count <= website_count:
-            positions = self.website.get_positions(rows=len_count, start=start_count)['response']['docs']
-            for doc in positions:
-                data.append((doc['REC_KEY'], doc['EA_ISBN']))
-            start_count += self.STEP
-            len_count = website_count - start_count
-            if len_count > self.STEP or len_count < 0:
-                len_count = self.STEP
-            if os.getenv('DEBUG') == 'True':
-                print('Filling database... (' + str(start_count) + '/' + str(website_count) + ')  -  ' + str(
-                    start_count / website_count * 100)[:4:] + '%')
-        self.database.reload(data)
+    def __database_filling__(self):
+        # start_count = 0
+        # len_count = website_count - start_count
+        # if len_count > self.STEP:
+        #     len_count = self.STEP
+        # data = []
+        # while start_count + len_count <= website_count:
+        #     positions = self.website.get_positions(rows=len_count, start=start_count)['response']['docs']
+        #     for doc in positions:
+        #         data.append((doc['REC_KEY'], doc['EA_ISBN']))
+        #     start_count += self.STEP
+        #     len_count = website_count - start_count
+        #     if len_count > self.STEP or len_count < 0:
+        #         len_count = self.STEP
+        #     if os.getenv('DEBUG') == 'True':
+        #         print('Filling database... (' + str(start_count) + '/' + str(website_count) + ')  -  ' + str(
+        #             start_count / website_count * 100)[:4:] + '%')
+        # self.database.reload(data)
+        self.database = self.website.get_positions()
 
-    def book_registration_notifier(self, key, isbn, name, success: bool = True):
-        message = 'Новый элемент: \n' + name + '\n' + (self.view_url + isbn) + '\n\n' + '✅ Найден'
-        if success:
-            message += '\n✅ Зарегистрирован'
-        else:
-            message += '\n❌ Зарегистрирован'
-            self.database.remove(key)
-            message += '\n✅ Удалён из базы данных'
+    def book_registration_notifier(self, url):
+        message = '✅ Новый элемент: \n\n' + url
         self.telegram.write(message, 'prod-main')
 
-    def book_registration(self, name, key, isbn):
-        try:
-            success = self.registrar.book_registration(self.view_url + isbn, name)
-        except Exception as exception:
-            self.book_registration_notifier(key, isbn, name, False)
-            raise exception
-        self.book_registration_notifier(key, isbn, name, success)
+    def book_registration(self, url):
+        # self.registrar.book_registration(tag)
+        self.database.append(url)
+        self.book_registration_notifier(url)
 
-    def update(self, is_first_update):
-        website_count = self.website.get_count()
-        database_count = self.database.count()
-        if database_count == 0 or website_count < database_count:
-            self.is_database_filled = False
-            self.__database_filling__(website_count)
-            self.is_database_filled = True
-        elif database_count < website_count:
-            start_count = 0
-            len_count = website_count - start_count
-            if len_count > self.STEP:
-                len_count = self.STEP
-            while start_count + len_count <= website_count and len_count >= 0:
-                positions = self.website.get_positions(rows=len_count, start=start_count)['response']['docs']
-                for doc in positions:
-                    if self.database.is_unique(doc['REC_KEY'], doc['EA_ISBN']):
-                        if not is_first_update:
-                            self.book_registration(doc['TITLE'], doc['REC_KEY'], doc['EA_ISBN'])
-                start_count += self.STEP
-                len_count = website_count - start_count
-                if len_count > self.STEP:
-                    len_count = self.STEP
-            self.database.commit()
+    def update(self):
+        # website_count = self.website.get_count()
+        database_count = len(self.database)
+        if database_count == 0:
+            # self.is_database_filled = False
+            self.__database_filling__()
+            # self.is_database_filled = True
+        # elif database_count < website_count:
+        #     start_count = 0
+        #     len_count = website_count - start_count
+        #     if len_count > self.STEP:
+        #         len_count = self.STEP
+        #     while start_count + len_count <= website_count and len_count >= 0:
+        positions = self.website.get_positions()
+        for doc in positions:
+            if doc not in self.database:
+                self.book_registration(doc)
+                # start_count += self.STEP
+            #     len_count = website_count - start_count
+            #     if len_count > self.STEP:
+            #         len_count = self.STEP
+            # self.database.commit()
 
     def start(self):
         is_start = True
@@ -101,7 +89,8 @@ class Controller:
             try:
                 if is_start:
                     self.telegram.write('Module launched', tag='prod-dev')
-                self.update(is_start)
+                self.update()
+                # self.database.remove('https://www.novelupdates.com/series/my-ex-boyfriend-became-my-current-teammate/')
                 if is_start:
                     self.telegram.write('Module started', tag='prod-dev')
                 is_start = False
--- modules/website.py
+++ modules/website.py
@@ -1,76 +1,94 @@
-import time
-import datetime
 from json.decoder import JSONDecodeError
+from bs4 import BeautifulSoup
+import cloudscraper
 
 import requests
 
-from modules.proxy import Proxy
-
 
 class Website:
-    def __init__(self, config, proxy_file_path):
+    def __init__(self, config):
         self.url_address = config['BASE_URL']
         self.search_url = config['SEARCH_URL']
-        self.search_request = config['SEARCH_REQUEST']
-        self.proxy = Proxy(proxy_file_path=proxy_file_path)
-        self.dynamic_date_search = config['DYNAMIC_DATE_SEARCH'] == 'True'
+        # self.search_request = config['SEARCH_REQUEST']
+        # self.proxy = Proxy(proxy_file_path=proxy_file_path)
+        # self.dynamic_date_search = config['DYNAMIC_DATE_SEARCH'] == 'True'
 
-    def __request__(self, rows: int, start: int = 0, page: int = 1) -> requests.Response:
-        fq_date: str = ''
-        if self.dynamic_date_search:
-            # fq_date = 'PUBLISH_PREDATE : [' + (datetime.date.today() - datetime.timedelta(days=35)).strftime(
-            #     '%Y/%m/%d') + ' TO ' + datetime.date.today().strftime('%Y/12/31') + ']'
-            fq_date = 'PUBLISH_PREDATE : [' + '2021/05/01' + ' TO ' + '2021/12/31' + ']'
+    def __request__(self) -> requests.Response:
         data = {
-            'wt': 'json',
-            'rows': rows,
-            'start': start,
-            'page': page,
-            'tSrch_total': self.search_request,
-            'fq_select': 'tSrc_total',
-            'fq': fq_date,
-            'detailSearchYn': 'Y',
+            #     'wt': 'json',
+            #     'rows': rows,
+            #     'start': start,
+            #     'page': page,
+            #     'tSrch_total': self.search_request,
+            #     'fq_select': 'tSrc_total',
+            #     'fq': fq_date,
+            #     'detailSearchYn': 'Y',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'ru,en;q=0.9',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 YaBrowser/21.8.0.1967 (beta) Yowser/2.5 Safari/537.36',
+            'cache-control': 'no-cache',
+            'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Yandex";v="92"',
+            'cookie': '_ga=GA1.2.294598963.1631647379; _gid=GA1.2.1703054048.1632238869; __cf_bm=ZLfia.3F.x.R_9UpWNRAHQM3gJ3eDntA1pg7FhQxN_g-1632241072-0-AX/QPEYX73ealMljfHuiAg3BlrEun8tBnIfIHoFV+ltx8ozZCUqAlro7npnXH3//5zeU34l+712IMs3eneSCZoBvBEctHhHksd8UBOrg/w2jDExautEkVYYc2Ioy67KWTg==',
+            'dnt': '1',
+            'pragma': 'no-cache',
+            'referer': 'https://www.novelupdates.com/',
+            'sec-ch-ua-mobile': '?0',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'cross-site',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+        }
+        headers = {
+            'report-to': '{"endpoints":[{"url":"https:\/\/a.nel.cloudflare.com\/report\/v3?s=%2F2KMBs36rLMD1y5I7M5x7L1CNxXbY9h6dQISona0zvfBIEDeHd0JZieYhPbY57vU1GuUx9JtlTSNlUZmkTniRHDrcwM3I%2FnvlQPZRCftDtT3%2FA2gQoidqSAcJGrsYS%2B88cGeQZGUjKhdrJnXdTqzJzft"}],"group":"cf-nel","max_age":604800}',
+            'expect-ct': 'max-age=604800, report-uri="https://report-uri.cloudflare.com/cdn-cgi/beacon/expect-ct"',
+
         }
         while True:
-            error_time = None
-            try:
-                response = requests.post(self.url_address + self.search_url, data=data,
-                                         proxies={'http': str(self.proxy)})
-                error_time = None
-                if response.ok:
-                    return response
-            except requests.exceptions.ConnectionError:
-                self.proxy.next()
-            except ConnectionError:
-                if error_time is None:
-                    error_time = time.time()
-                if error_time is not None:
-                    if error_time - time.time() >= 300:
-                        self.proxy.next()
+            # error_time = None
+            # try:
+            scraper = cloudscraper.create_scraper()
+            # response = requests.post(self.url_address + self.search_url, data=data, headers=headers)
+            response = scraper.get(self.url_address + self.search_url)
+            # error_time = None
+            if response.ok:
+                return response
+            # except requests.exceptions.ConnectionError:
+            #     self.proxy.next()
+            # except ConnectionError:
+            #     if error_time is None:
+            #         error_time = time.time()
+            #     if error_time is not None:
+            #         if error_time - time.time() >= 300:
+            #             self.proxy.next()
 
-    def get_positions(self, rows: int = None, start: int = 0, page: int = 1):
-        if rows is None:
-            rows = self.get_count()
-        error_counter = 0
+    def get_positions(self):
+        # error_counter = 0
         while True:
-            try:
-                result = self.__request__(rows=rows, start=start, page=page).json()
-                return result
-            except JSONDecodeError:
-                error_counter += 1
-                if error_counter >= 10:
-                    raise ConnectionError('Ошибка подключения к seoji')
+            # try:
+            result = []
+            soup = BeautifulSoup(self.__request__().text, 'html.parser')
+            for data in soup.find_all(class_='search_title'):
+                for child in data.children:
+                    if 'href' in child.attrs:
+                        result.append(child.attrs['href'])
+            return result
+        # except JSONDecodeError:
+        #     error_counter += 1
+        #     if error_counter >= 10:
+        #         raise ConnectionError('Ошибка подключения к novelupdates')
 
-    def get_count(self) -> int:
-        error_counter = 0
-        while True:
-            try:
-                result = self.__request__(rows=0)
-                return int(result.json()['response']['numFound'])
-            except JSONDecodeError:
-                error_counter += 1
-                if error_counter >= 10:
-                    raise ConnectionError('Ошибка подключения к seoji')
+    # def get_count(self) -> int:
+    #     error_counter = 0
+    #     while True:
+    #         try:
+    #             result = self.__request__(rows=0)
+    #             return int(result.json()['response']['numFound'])
+    #         except JSONDecodeError:
+    #             error_counter += 1
+    #             if error_counter >= 10:
+    #                 raise ConnectionError('Ошибка подключения к seoji')
 
-    def update_proxy(self, val):
-        self.proxy = val
+    # def update_proxy(self, val):
+    #     self.proxy = val
